4eea873d71265e9607578f816b340468e9c8abb7
Arlandrian <numan_kazanci@hotmail.com>
2022-11-10 03:35:44 +0300

    minor change

--- youtube/youtubeService.js
+++ youtube/youtubeService.js
@@ -52,7 +52,7 @@ const validateChannelId = async (channelId) =>{
     part:["id","auditDetails","contentOwnerDetails"],
     id: channelId
   }).catch(err => 
-    console.log(err)
+    console.error(err)
   )
   return response.data.pageInfo.totalResults != 0
 }
