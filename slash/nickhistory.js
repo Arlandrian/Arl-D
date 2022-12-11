fd9fb5727db94a77dff0cdb60a76b560a3203948
arlandrian <alo_istanbul@hotmail.com>
2022-12-11 17:32:34 +0100

    refactor for clean code

--- slash/nickhistory.js
+++ slash/nickhistory.js
@@ -1,28 +1,28 @@
-const discord = require("discord.js"); 
+const discord = require("discord.js");
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   //await interaction.deferReply({ephemeral: true});
-  await interaction.deferReply({ephemeral: false});
+  await interaction.deferReply({ ephemeral: false });
 
   const { db } = client;
-  let channels =  await db.getNickLogChannels(interaction.guild.id)
-  if(channels.length < 0){
+  let channels = await db.getNickLogChannels(interaction.guild.id)
+  if (channels.length < 0) {
     await interaction.editReply("Couldnt get log channels from db");
     return
   }
 
   let logChannelId = channels[0]
-  if(logChannelId == null){
+  if (logChannelId == null) {
     await interaction.editReply("There is no log channels set!");
     return
   }
 
   let logChannel = client.guilds.cache.get(interaction.guildId)?.channels?.cache?.get(logChannelId);
-  if(logChannel == null){
+  if (logChannel == null) {
     logChannel = client.guilds.cache.get(interaction.guildId).channels.fetch(logChannelId)
   }
 
-  if(logChannel == null){
+  if (logChannel == null) {
     await interaction.editReply("Couldnt find log channel!");
     return
   }
@@ -30,27 +30,27 @@ exports.run = async (client, interaction) => { // eslint-disable-line no-unused-
   let user = interaction.targetUser;
   messages = await getAllMessagesInTheChannel(logChannel)
 
-  let messagesContainingId = messages.filter(x=>x[1].content.includes(user.id))
-  let reply = messagesContainingId.map(x=>x[1].content).join('\n');
-  if(messagesContainingId.length == 0){
+  let messagesContainingId = messages.filter(x => x[1].content.includes(user.id))
+  let reply = messagesContainingId.map(x => x[1].content).join('\n');
+  if (messagesContainingId.length == 0) {
     reply = "There is no log for this user."
   }
   reply += '\n'
   reply += `\nThere are ${messages.length} messages in the nick log channel.\n`
 
-  if(reply.length < 2000){
+  if (reply.length < 2000) {
     const rep = await interaction.editReply(reply);
-  }else{
+  } else {
     await sendMessageAsTextFileAttachment(interaction, reply)
   }
 };
 
-async function sendMessageAsTextFileAttachment(interaction, content){
+async function sendMessageAsTextFileAttachment(interaction, content) {
   let atc = new discord.MessageAttachment(Buffer.from(content), 'nicklog.txt');
   await interaction.editReply({ files: [atc] });
 }
 
-async function getAllMessagesInTheChannel(channel){
+async function getAllMessagesInTheChannel(channel) {
   return await lots_of_messages_getter(channel)
   /*
   if(channel.isAllMessagesCached == null){
@@ -62,25 +62,25 @@ async function getAllMessagesInTheChannel(channel){
 }
 
 async function lots_of_messages_getter(channel, limit = 2000) {
-    const sum_messages = [];
-    let last_id;
+  const sum_messages = [];
+  let last_id;
 
-    while (true) {
-        const options = { limit: 100, cache: true };
-        if (last_id) {
-            options.before = last_id;
-        }
-
-        const messages = await channel.messages.fetch(options);
-        sum_messages.push(...messages);
-        last_id = messages.last().id;
-
-        if (messages.size != 100 || sum_messages >= limit) {
-            break;
-        }
+  while (true) {
+    const options = { limit: 100, cache: true };
+    if (last_id) {
+      options.before = last_id;
     }
 
-    return sum_messages;
+    const messages = await channel.messages.fetch(options);
+    sum_messages.push(...messages);
+    last_id = messages.last().id;
+
+    if (messages.size != 100 || sum_messages >= limit) {
+      break;
+    }
+  }
+
+  return sum_messages;
 }
 
 exports.commandData = {
