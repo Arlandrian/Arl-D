baaa5e311db06b7f00942c4efb9eff1fbf2dd60e
arlandrian <alo_istanbul@hotmail.com>
2022-12-13 15:42:15 +0100

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 # kill running process if it exists
 ps aux | grep "node index.js" | grep -vE grep |  awk '{print $2}' | xargs -r kill
+
+sleep 3
+
 # start new process
 nohup node index.js > arl_d.log 2>&1 & echo $! > pid
 
