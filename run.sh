4670b4a8455307a81e0e1f32d27bec94f2876d91
arlandrian <alo_istanbul@hotmail.com>
2022-12-13 17:16:14 +0100

    wf test

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # kill running process if it exists
 ps aux | grep "node index.js" | grep -vE grep |  awk '{print $2}' | xargs -r kill
 
-sleep 3
+sleep 2
 
 # start new process
 nohup node index.js > arl_d.log 2>&1 & echo $! > pid
-
+sleep 10
 echo "The deployment is complete"
