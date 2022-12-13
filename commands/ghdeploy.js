1f20cb8fdbeb46c2202ea622b6e818a54704a99b
arlandrian <alo_istanbul@hotmail.com>
2022-12-13 18:12:36 +0100

    AAAAAAAAAAAAAAA

--- commands/ghdeploy.js
+++ commands/ghdeploy.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const config = require("../config.js");
 const { settings } = require("../modules/settings.js");
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const replying = settings.ensure(message.guild.id, config.defaultSettings).commandReply;
