cd05980275c715835d89b5f7f5a35aa2863d834c
arlandrian <alo_istanbul@hotmail.com>
2022-12-22 16:34:32 +0100

    added admin message command

---
+++ slash/adminMessage.js
@@ -0,0 +1,42 @@
+const discord = require("discord.js");
+const logger = require("../modules/logger")
+exports.run = async (client, interaction) => { 
+  await interaction.deferReply({ ephemeral: true });
+  let opts = interaction.options._hoistedOptions;
+  if(opts[0].value == null){
+    await interaction.editReply("Please enter a message")
+    return;
+  }
+  let msg = opts[0].value
+  // Create embed message
+  let embedMessage = new discord.MessageEmbed()
+    .setDescription(msg)
+    .setColor("#e9be11")
+  interaction.channel.send({embeds:[embedMessage]})
+  logger.log(`Admin:${interaction.user.tag} | Message:${msg}`)
+  await interaction.editReply("Admin message is sent.")
+};
+
+exports.commandData = {
+  name: "Admin Message",
+  description: "Sends an admin message.",
+  descriptionLocalizations: "",
+  options: [
+    {
+      "name": "message",
+      "description": "Message",
+      "type": 3,
+      "required": true
+    }
+  ],
+  defaultMemberPermissions: discord.Permissions.FLAGS.BAN_MEMBERS,
+  dmPermission: false,
+  type: 1
+};
+
+// Set guildOnly to true if you want it to be available on guilds only.
+// Otherwise false is global.
+exports.conf = {
+  permLevel: "User",
+  guildOnly: true
+};
