5bacdde8137ef6e453080f11bb1b8cb9288edfc9
arlandrian <alo_istanbul@hotmail.com>
2022-12-26 20:03:58 +0100

    refactor on database

--- modules/database.js
+++ modules/database.js
@@ -1,13 +1,17 @@
 const { createClient } = require('redis');
 let client = null
 async function initdb(){
+  if(process.env.REDIS_URL == null){
+    console.warn("REDIS_URL env var is not defined, some features will not work!")
+    return
+  }
+
   //'redis://alice:foobared@awesome.redis.server:6380'
   client = createClient({
     url: `redis://${process.env.REDIS_USER}:${process.env.REDIS_PWD}@${process.env.REDIS_URL}`,
   });
   client.on('error', (err) => console.log('Redis Client Error', err));
   client.on('connection', (me) => console.log('Redis connected successfully.'));
-  
   await client.connect();
 }
 
@@ -15,42 +19,47 @@ const NICK_LOG_KEY = "arld:guild:nickLogChannels"
 const CONFESSION_KEY = "arld:guild:confessionChannels"
 const YOUTUBE_BOT_CONFIG_KEY = "arld:youtubeBotConfig"
 
+async function setGuildChannelsBase(channelKey, guildId, channels) {
+  await client.hSet(channelKey, guildId, JSON.stringify(channels))
+}
+
+async function getGuildChannelsBase(channelKey, guildId){
+  let val = await client.hGet(channelKey, guildId)
+  if(val == null){
+    val = []
+    await setGuildChannelsBase(channelKey, guildId, val)
+    return val
+  }
+  return JSON.parse(val)
+}
+
+async function getAllGuildChannelsBase(channelKey){
+  let data = await client.hGetAll(channelKey)
+  return Object.keys(data).map(key => ({ key, value: JSON.parse(data[key]) }))
+}
+
 async function setNickLogChannels(guildId, channels) {
-  await client.hSet(NICK_LOG_KEY, guildId, JSON.stringify(channels))
+  await setGuildChannelsBase(NICK_LOG_KEY, guildId, channels)
 }
 
 async function getNickLogChannels(guildId) {
-  let val = await client.hGet(NICK_LOG_KEY, guildId)
-  if(val == null){
-    val = []
-    await setNickLogChannels(guildId, val)
-    return val
-  }
-  return JSON.parse(val)
+  return await getGuildChannelsBase(NICK_LOG_KEY, guildId)
 }
 
 async function getAllNickLogChannels(){
-  let data = await client.hGetAll(NICK_LOG_KEY)
-  return Object.keys(data).map(key => ({ key, value: JSON.parse(data[key]) }))
+  return await getAllGuildChannelsBase(NICK_LOG_KEY)
 }
 
 async function setConfessionChannels(guildId, channels) {
-  await client.hSet(CONFESSION_KEY, guildId, JSON.stringify(channels))
+  await setGuildChannelsBase(CONFESSION_KEY, guildId, channels)
 }
 
 async function getConfessionChannels(guildId) {
-  let val = await client.hGet(CONFESSION_KEY, guildId)
-  if(val == null){
-    val = []
-    await setConfessionChannels(guildId, val)
-    return val
-  }
-  return JSON.parse(val)
+  return await getGuildChannelsBase(CONFESSION_KEY, guildId)
 }
 
 async function getAllConfessionChannels(){
-  let data = await client.hGetAll(CONFESSION_KEY)
-  return Object.keys(data).map(key => ({ key, value: JSON.parse(data[key]) }))
+  return await getAllGuildChannelsBase(CONFESSION_KEY)
 }
 
 async function getYoutubeBotConfig() {
@@ -58,6 +67,22 @@ async function getYoutubeBotConfig() {
 }
 async function setYoutubeBotConfig(config) {
   await client.set(YOUTUBE_BOT_CONFIG_KEY, JSON.stringify(config))
+}
+
+
+// Base Helper
+async function saveGuildObject(guildId, key, value){
+  await client.hSet(key, guildId, JSON.stringify(value))
+}
+
+async function loadGuildObject(guildId, key){
+  let val = await client.hGet(key, guildId)
+  if(val == null){
+    val = {}
+    await saveGuildObject(key, guildId, val)
+    return val
+  }
+  return JSON.parse(val)
 }
 
 module.exports = { 
