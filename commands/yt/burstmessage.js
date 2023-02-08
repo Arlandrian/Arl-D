37cfdfd847da60d6a35753d617536c0f4b76331e
Numan Kemal Kazancı <alo_istanbul@hotmail.com>
2023-02-08 01:47:44 +0100

    burstmsg Promise.all

--- commands/yt/burstmessage.js
+++ commands/yt/burstmessage.js
@@ -7,22 +7,29 @@ const durationFormatter = new DurationFormatter();
 exports.run = async (client, message, args, level) => { 
   let burstCount = Number(args[0])
   // Check if valid arg
-  if(isNaN(burstCount) || burstCount == null){
+  if(isNaN(burstCount) || burstCount == null || burstCount < 1){
     message.reply("invalid input");
     return
   }
 
   const t0 = performance.now();
   let msg = args.slice(1).join(' ');
+  let promises = []
   for(let i=0; i < burstCount; i++){
-    let resp = await youtube.sendLiveChatMessage(msg);
-    if(resp.error != null) {
-      message.reply(resp.error)
+    promises.push(youtube.sendLiveChatMessage(msg));
+  }
+
+  let results = await Promise.allSettled(promises)
+
+  for(let i=0; i < results.length; i++){
+    let res = results[i]
+    if(res.error != null) {
+      message.reply(res.error)
       return
     }
   }
+
   const t1 = performance.now();
-  
   const reply = `${burstCount} messages sent in ${(t1-t0)} milliseconds.`
   message.reply(reply);
 };
