fc85385584aae455722c1226f1964d0e3521842d
Numan Kemal Kazancı <alo_istanbul@hotmail.com>
2023-03-11 23:48:27 +0100

    bazal type change

--- slash/bazal.js
+++ slash/bazal.js
@@ -3,9 +3,9 @@ const logger = require("../modules/logger")
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   await interaction.deferReply({ ephemeral: true });
   let opts = interaction.options._hoistedOptions;
-  let boy = opts[0]
-  let kilo = opts[1]
-  let yas = opts[2]
+  let boy = number(opts[0])
+  let kilo = number(opts[1])
+  let yas = number(opts[2])
   let bazalMetabolizma = (13.75*kilo) + (5.03*boy) - (6.75*yas) + 66.5
   await interaction.editReply(`Bazal metabilizma hızınız: ${bazalMetabolizma}`);
 };
@@ -17,19 +17,19 @@ exports.commandData = {
     {
       "name": "Boy",
       "description": "Boyunuzu santimetre cinsinden giriniz.",
-      "type": 4,
+      "type": 3,
       "required": true
     },
     {
       "name": "Kilo",
       "description": "Kilonuzu kilogram cinsinden giriniz.",
-      "type": 4,
+      "type": 3,
       "required": true
     },
     {
       "name": "Yas",
       "description": "Yaşınızı giriniz.",
-      "type": 4,
+      "type": 3,
       "required": true
     }
   ],
