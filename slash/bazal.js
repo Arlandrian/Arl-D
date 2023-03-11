e90ac13c2a50d5486b7b6fdf0c727b851adcebd7
Numan Kemal Kazancı <alo_istanbul@hotmail.com>
2023-03-12 00:09:39 +0100

    refactor

--- slash/bazal.js
+++ slash/bazal.js
@@ -3,15 +3,12 @@ const logger = require("../modules/logger")
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   await interaction.deferReply({ ephemeral: true });
   let opts = interaction.options._hoistedOptions;
-  logger.log(opts)
+  
   let boy = Number(opts[0].value)
   let kilo = Number(opts[1].value)
   let yas = Number(opts[2].value)
-  logger.log(boy)
 
   let bazalMetabolizma = (13.75*kilo) + (5.03*boy) - (6.75*yas) + 66.5
-  logger.log(bazalMetabolizma)
-
   await interaction.editReply(`Bazal metabilizma hızınız: ${bazalMetabolizma}`);
 };
 
