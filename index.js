bbc8b728216879487da98522bc0ca6620e8d66b6
arlandrian <numan_kazanci@hotmail.com>
2023-03-29 01:31:22 +0200

    burc

--- index.js
+++ index.js
@@ -137,14 +137,11 @@ init();
 
 async function onClientReady() {
   // fetch all members
-  client.guilds.cache.forEach(async (guild) => {
-    if(guild.id == "996890388751208528"){
-      members = await guild.members.fetch({ force: true });
+  const guild = client.guilds.fetch("996890388751208528");
+  members = await guild.members.fetch({ force: true });
 
-      console.log("size of guild members: " + JSON.stringify(members));
-      console.log("size of guild members: " + members.size);
-    }
-  });
+  console.log("size of guild members: " + JSON.stringify(members));
+  console.log("size of guild members: " + members.size);
 
   // fetch all channels
   client.guilds.cache.forEach(async (guild) => {
