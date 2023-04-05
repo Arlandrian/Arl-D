4424dd741321e2c007a8a8082cef085ea608dd63
arlandrian <numan_kazanci@hotmail.com>
2023-04-05 06:04:36 +0200

    test

--- slash/videoedit.js
+++ slash/videoedit.js
@@ -3,6 +3,7 @@ const discord = require("discord.js");
 const logger = require("../modules/logger")
 const videoedit = require("../modules/videoedit")
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
+  return
   await interaction.deferReply({ ephemeral: false });
 
   let opts=interaction.options._hoistedOptions
@@ -28,56 +29,56 @@ exports.commandData = {
   description: "Cut a video and audio from youtube.",
   // descriptionLocalizations: {},
   options: [
-    {
-      "name": "videoURL",
-      "description": "video youtube url",
-      // "description": "Videonun linki. (sadece youtube)",
-      "type": 3, 
-      "required": true
-    },
-    {
-      "name": "audioURL",
-      "description": "Audio youtube url",
-      // "description": "Sesin linki. (sadece youtube)",
-      "type": 3,
-      "required": true
-    },
-    {
-      "name": "videoStartSec",
-      "description": "video start point for the cut",
-      // "description": "videoda kesilmeye baslanacak noktanin saniyesi",
-      "type": 4,
-      "required": true,
-      "min_value": 0,
-      "max_value": 7200
-    },
-    {
-      "name": "videoEndSec",
-      "description": "video end point for the cut",
-      // "description": "videoda kesilmenin biteceği noktanin saniyesi",
-      "type": 4,
-      "required": true,
-      "min_value": 0,
-      "max_value": 7200
-    },
-    {
-      "name": "audioStartSec",
-      "description": "audio start point for the cut",
-      // "description": "sesde kesilmeye baslanacak noktanin saniyesi",
-      "type": 4,
-      "required": true,
-      "min_value": 0,
-      "max_value": 7200
-    },
-    {
-      "name": "audioEndSec",
-      "description": "audio end point for the cut",
-      // "description": "sesde kesilmenin biteceği noktanin saniyesi",
-      "type": 4,
-      "required": true,
-      "min_value": 0,
-      "max_value": 7200
-    }
+    // {
+    //   "name": "videoURL",
+    //   "description": "video youtube url",
+    //   // "description": "Videonun linki. (sadece youtube)",
+    //   "type": 3, 
+    //   "required": true
+    // },
+    // {
+    //   "name": "audioURL",
+    //   "description": "Audio youtube url",
+    //   // "description": "Sesin linki. (sadece youtube)",
+    //   "type": 3,
+    //   "required": true
+    // },
+    // {
+    //   "name": "videoStartSec",
+    //   "description": "video start point for the cut",
+    //   // "description": "videoda kesilmeye baslanacak noktanin saniyesi",
+    //   "type": 4,
+    //   "required": true,
+    //   "min_value": 0,
+    //   "max_value": 7200
+    // },
+    // {
+    //   "name": "videoEndSec",
+    //   "description": "video end point for the cut",
+    //   // "description": "videoda kesilmenin biteceği noktanin saniyesi",
+    //   "type": 4,
+    //   "required": true,
+    //   "min_value": 0,
+    //   "max_value": 7200
+    // },
+    // {
+    //   "name": "audioStartSec",
+    //   "description": "audio start point for the cut",
+    //   // "description": "sesde kesilmeye baslanacak noktanin saniyesi",
+    //   "type": 4,
+    //   "required": true,
+    //   "min_value": 0,
+    //   "max_value": 7200
+    // },
+    // {
+    //   "name": "audioEndSec",
+    //   "description": "audio end point for the cut",
+    //   // "description": "sesde kesilmenin biteceği noktanin saniyesi",
+    //   "type": 4,
+    //   "required": true,
+    //   "min_value": 0,
+    //   "max_value": 7200
+    // }
   ],
   defaultPermission: true,
   type: 1
