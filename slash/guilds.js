de16e5c0147a579879d1ea94b8920ac22041b9e1
arlandrian <numan_kazanci@hotmail.com>
2023-11-10 00:58:56 +0300

    fixed guilds command

--- slash/guilds.js
+++ slash/guilds.js
@@ -2,20 +2,21 @@ const discord = require("discord.js");
 exports.run = async (client, interaction) => {
   // eslint-disable-line no-unused-vars
   await interaction.deferReply({ ephemeral: false });
-  const guilds = client.guilds.cache.array();
   let content = "";
-  for (const guild in guilds) {
+  client.guilds.fetch();
+  client.guilds.cache.forEach((guild) => {
     content += `\n# ${guild.name}'${guild.id}'\nmembers:${guild.memberCount}, channels: ${guild.channels.cache.size} - Owner: ${guild.owner.user.tag} (${guild.ownerID})\n`;
     content += "## Channels\n";
-    for (const ch in guild.channels.cache.array()) {
+    guild.channels.cache.forEach((ch) => {
       content += `  ${ch.name}${ch.isThread() ? " (Thread)" : ""}\n`;
-    }
+    });
     content += "## Roles\n";
-    for (const role in guild.roles.cache.array()) {
+    guild.roles.cache.forEach((role) => {
       content += `  ${role.name}\n`;
-    }
+    });
     content += "\n---\n";
-  }
+  });
+
   const atc = new discord.MessageAttachment(
     Buffer.from(content),
     "guilds_export.md"
