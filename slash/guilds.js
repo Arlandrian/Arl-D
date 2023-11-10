93db26ffa1b9ef2c5d596642ce3ea2f9c489df73
arlandrian <numan_kazanci@hotmail.com>
2023-11-10 03:11:57 +0300

    added subcommands unfinished

--- slash/guilds.js
+++ slash/guilds.js
@@ -2,6 +2,111 @@ const discord = require("discord.js");
 exports.run = async (client, interaction) => {
   // eslint-disable-line no-unused-vars
   await interaction.deferReply({ ephemeral: false });
+
+  switch (interaction.options.getSubcommand()) {
+    case "getall":
+      await getAllHandler(client, interaction);
+      break;
+    case "readchannel":
+      await readChannelHandler(client, interaction);
+      break;
+    case "writechannel":
+      await writeChannelHandler(client, interaction);
+      break;
+    case "listenchannel":
+      await listenChannelHandler(client, interaction);
+      break;
+  }
+};
+
+exports.commandData = {
+  name: "guilds",
+  description: "Exports information about the guilds that using this bot.",
+  defaultPermission: true,
+  type: 1, //ApplicationCommandTypes.USER
+  options: [
+    {
+      name: "getall",
+      description: "Exports information about the guilds that using this bot.",
+      type: "SUB_COMMAND",
+      options: [],
+    },
+    {
+      name: "readchannel",
+      description: "read messages in a guild channel",
+      descriptionLocalizations: {
+        tr: "Bir sunucudaki mesajları oku.",
+      },
+      type: "SUB_COMMAND",
+      options: [
+        ...requiredSubCommandOptions,
+        {
+          name: "count",
+          description: "Number of message to read",
+          descriptionLocalizations: {
+            tr: "Okunacak mesaj sayısı",
+          },
+          type: 4,
+          required: false,
+          min_value: 0,
+          max_value: 1000,
+        },
+      ],
+    },
+    {
+      name: "writechannel",
+      description: "listen messages in a guild channel",
+      descriptionLocalizations: {
+        tr: "Bir sunucudaki mesajları dinler.",
+      },
+      type: "SUB_COMMAND",
+      options: [
+        ...requiredSubCommandOptions,
+        {
+          name: "message",
+          description: "content of the message",
+          descriptionLocalizations: {
+            tr: "Mesaj içeriği.",
+          },
+          type: 3,
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "listenchannel",
+      description: "listen messages in a guild channel",
+      descriptionLocalizations: {
+        tr: "Bir sunucudaki mesajları dinler.",
+      },
+      type: "SUB_COMMAND",
+      options: [...requiredSubCommandOptions],
+    },
+  ],
+};
+
+const requiredSubCommandOptions = [
+  {
+    name: "guildid",
+    description: "Id of guild.",
+    descriptionLocalizations: {
+      tr: "Server id",
+    },
+    type: 3,
+    required: true,
+  },
+  {
+    name: "channelid",
+    description: "Id of channel",
+    descriptionLocalizations: {
+      tr: "id channel",
+    },
+    type: 3,
+    required: true,
+  },
+];
+
+async function getAllHandler(client, interaction) {
   let content = "# GUILDS";
   await client.guilds.fetch();
   content += client.guilds.cache.size + "\n";
@@ -28,15 +133,64 @@ exports.run = async (client, interaction) => {
     "guilds_export.md"
   );
   interaction.editReply({ files: [atc] });
-};
+}
 
-exports.commandData = {
-  name: "guilds",
-  description: "Exports information about the guilds that using this bot.",
-  options: [],
-  defaultPermission: true,
-  type: 1, //ApplicationCommandTypes.USER
-};
+function readChannelHandler(client, interaction) {
+  const opts = interaction.options._hoistedOptions;
+  const guildId = opts[0].value;
+  const channelId = opts[1].value;
+  const messageCount = getOption(opts, "count", "100");
+  interaction.editReply("not implemented");
+}
+
+function writeChannelHandler(client, interaction) {
+  const opts = interaction.options._hoistedOptions;
+  const guildId = opts[0].value;
+  const channelId = opts[1].value;
+  const messageContent = opts[2].value;
+}
+
+function listenChannelHandler(client, interaction) {
+  const opts = interaction.options._hoistedOptions;
+  const guildId = opts[0].value;
+  const channelId = opts[1].value;
+  client.on("messageCreate", (message) => {
+    // you dont need to listen messages for the same channel, you are already see the messages on that channel
+    if (isSameChannel(message, interaction)) {
+      return;
+    }
+    if (guildId == "all") {
+      if (channelId == "all") {
+        interaction.channel.send(message);
+      } else if (channelId == message.channel.id) {
+        interaction.channel.send(message);
+      }
+      return;
+    }
+    if (message.guildId == guildId) {
+      if (channelId == "all") {
+        interaction.channel.send(message);
+      } else if (channelId == message.channel.id) {
+        interaction.channel.send(message);
+      }
+      return;
+    }
+  });
+  interaction.editReply("started listening channel - please dont abuse this");
+}
+
+function isSameChannel(message, interaction) {
+  return message.channel.id == interaction.channel.id;
+}
+
+function getOption(opts, name, defValue) {
+  const provided = opts.find((x) => x.name == name);
+  if (provided == null) {
+    return defValue;
+  } else {
+    return provided.value;
+  }
+}
 
 exports.conf = {
   permLevel: "Bot Owner",
