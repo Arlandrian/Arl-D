18073f309a0fd68607fd0ffe357fef670da2d64d
arlandrian <numan_kazanci@hotmail.com>
2023-11-10 21:31:23 +0300

    updated gh deploy

--- commands/ghdeploy.js
+++ commands/ghdeploy.js
@@ -1,75 +1,69 @@
-const axios = require('axios');
+const axios = require("axios");
 const config = require("../config.js");
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const replying = true
-  let branch = "master"
-  if(args.length > 0){
-    branch = args[0]
+exports.run = async (client, message, args, level) => {
+  // eslint-disable-line no-unused-vars
+  const replying = true;
+  let branch = "master";
+  if (args.length > 0) {
+    branch = args[0];
   }
 
-  let result = await githubWorkflowTrigger(branch);
-  if(result==null){
-    message.reply(
-      { 
-        content: `Build started.`,
-        allowedMentions: { repliedUser: (replying === "true")},
-        ephemeral: true 
-      });
-  }else{
-    message.reply(
-      { content: `Failed to dispatch workflow! ${result.message}`,
-        allowedMentions: { repliedUser: (replying === "true") },
-        ephemeral: true
-      });
+  const result = await githubWorkflowTrigger(branch);
+  if (result == null) {
+    message.reply({
+      content: "Build started.",
+      allowedMentions: { repliedUser: replying === "true" },
+      ephemeral: true,
+    });
+  } else {
+    message.reply({
+      content: `Failed to dispatch workflow! ${result.message}`,
+      allowedMentions: { repliedUser: replying === "true" },
+      ephemeral: true,
+    });
   }
 };
 
 async function githubWorkflowTrigger(branch) {
-  const OWNER = process.env.MGITHUB_OWNER
-  const REPO = process.env.MGITHUB_REPO
-  const WORKFLOW_ID = "build-deploy.yml"
-  const GITHUB_TOKEN = process.env.MGITHUB_WORKFLOW_TOKEN
+  const OWNER = process.env.MGITHUB_OWNER;
+  const REPO = process.env.MGITHUB_REPO;
+  const WORKFLOW_ID = "build-deploy.yml";
+  const GITHUB_TOKEN = process.env.MGITHUB_WORKFLOW_TOKEN;
 
   const data = {
-    ref: branch
-    // ,
-    // inputs: {
-    //   "name": "Mona the Octocat",
-    //   "home": "San Francisco, CA"
-    // }
-  }
+    ref: branch,
+    inputs: {
+      skip_node_install: "true",
+    },
+  };
 
-  try{
-    const URL = `https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`
-    const response = await axios.post(
-      URL,
-      JSON.stringify(data),
-      {
-        headers: {
-          'Accept': 'application/vnd.github+json',
-          'Authorization': 'Bearer ' + GITHUB_TOKEN,
-          'X-GitHub-Api-Version': '2022-11-28',
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-    )
-    return null
-  }catch(err){
-    console.error(err)
-    return err
+  try {
+    const URL = `https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches`;
+    const response = await axios.post(URL, JSON.stringify(data), {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: "Bearer " + GITHUB_TOKEN,
+        "X-GitHub-Api-Version": "2022-11-28",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    return null;
+  } catch (err) {
+    console.error(err);
+    return err;
   }
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["ghdeploy","githubdeploy"],
-  permLevel: "Bot Owner"
+  aliases: ["ghdeploy", "githubdeploy"],
+  permLevel: "Bot Owner",
 };
 
 exports.help = {
   name: "ghdeploy",
   category: "System",
   description: "This will build and deploy the whole application from github.",
-  usage: "ghdeploy [branch]"
+  usage: "ghdeploy [branch]",
 };
