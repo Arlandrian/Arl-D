dfebd863c659460e533392d12ab5aec27950ee3c
arlandrian <alo_istanbul@hotmail.com>
2023-12-11 00:09:22 +0100

    regex fix

--- modules/videoedit.js
+++ modules/videoedit.js
@@ -337,7 +337,7 @@ function downloadYoutubeAudioAsync(url, outputPath) {
 
 function isValidFFmpegOpts(opts) {
   // Regex pattern for FFmpeg options
-  const ffmpegOptsRegex = /^[a-zA-Z0-9\[\]:-\s]+$/;
+  const ffmpegOptsRegex = /^[a-zA-Z0-9\[\]_:-\s]+$/;
   return ffmpegOptsRegex.test(opts);
 }
 
