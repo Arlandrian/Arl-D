f1dba883fdf647be496053a084fe4aceeb05aa42
arlandrian <alo_istanbul@hotmail.com>
2023-12-24 18:05:39 +0100

    fixed youtube no highest format found

--- modules/videoedit.js
+++ modules/videoedit.js
@@ -320,7 +320,7 @@ function downloadYoutubeVideoAsync(url, outputPath) {
       return (
         format.hasVideo &&
         !format.hasAudio &&
-        format.quality == 'medium' &&
+        (format.quality == 'medium' || format.quality == 'small'|| format.quality == 'tiny') &&
         format.approxDurationMs != null &&
         format.approxDurationMs < MAX_VIDEO_MS
       );
