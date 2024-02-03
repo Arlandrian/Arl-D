7f430b1eebf55e6b6fe5abef196d09d057f44cbe
arlandrian <alo_istanbul@hotmail.com>
2024-02-03 01:44:07 +0100

    sticker steal debug

--- slash/stickersteal.js
+++ slash/stickersteal.js
@@ -5,14 +5,18 @@ exports.run = async (client, interaction) => {
   await interaction.deferReply({ ephemeral: false });
   let message = interaction.targetMessage;
   const stickers = message.stickers;
+  console.log("Stickers:"+JSON.stringify(stickers))
   const firstSticker = stickers.first();
-
+  
   if (firstSticker) {
     console.log(firstSticker)
     const namee = firstSticker.name;
     message.guild.stickers.create(firstSticker.url, firstSticker.name, firstSticker.tags)
       .then(sticker => interaction.channel.send(`**${namee}** added successfully!`))
-      .catch(error => console.error(`Error adding sticker to the guild: ${error}`));
+      .catch(error => {
+        console.error(`Error adding sticker to the guild: ${error}`)
+        interaction.channel.send(`cant add **${namee}**! error: ${error}`)
+      });
   } else {
     interaction.editReply({
       content: "No stickers found in the message.",
