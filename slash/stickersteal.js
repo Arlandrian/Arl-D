c7263de955f30383d28b589b90961dc04cb438bd
arlandrian <alo_istanbul@hotmail.com>
2024-02-03 01:54:46 +0100

    sticker steal fix

--- slash/stickersteal.js
+++ slash/stickersteal.js
@@ -7,20 +7,30 @@ exports.run = async (client, interaction) => {
   const stickers = message.stickers;
   const firstSticker = stickers.first();
   if (firstSticker) {
-    firstSticker.fetch()
-    .then(fetchedSticker => {
-      // Add the fetched sticker to the guild
-      message.guild.stickers.create(fetchedSticker.url, fetchedSticker.name)
-        .then(sticker => interaction.channel.send(`**${firstSticker.name}** added successfully!`))
-        .catch(error => {
-          console.error(`Error adding sticker to the guild: ${error}`)
-          interaction.channel.send(`cant add **${namee}**! error: ${error}`)
-        });
-    })
-    .catch(error => {
-      console.error(`Error fetching sticker: ${error}`)
-      interaction.channel.send(`cant add **${namee}**! error: ${error}`)
-    });
+    firstSticker
+      .fetch()
+      .then((fetchedSticker) => {
+        // Add the fetched sticker to the guild
+        message.guild.stickers
+          .create(fetchedSticker.url, fetchedSticker.name)
+          .then((sticker) =>
+            interaction.channel.send(
+              `**${firstSticker.name}** added successfully!`
+            )
+          )
+          .catch((error) => {
+            console.error(`Error adding sticker to the guild: ${error}`);
+            interaction.channel.send(
+              `cant add **${firstSticker.name}**! error: ${error}`
+            );
+          });
+      })
+      .catch((error) => {
+        console.error(`Error fetching sticker: ${error}`);
+        interaction.channel.send(
+          `cant add **${firstSticker.name}**! error: ${error}`
+        );
+      });
   } else {
     interaction.editReply({
       content: "No stickers found in the message.",
@@ -28,7 +38,6 @@ exports.run = async (client, interaction) => {
     });
   }
 };
-
 
 exports.commandData = {
   name: "Sticker Steal",
@@ -38,7 +47,6 @@ exports.commandData = {
     discord.Permissions.FLAGS.MANAGE_EMOJIS_AND_STICKERS,
   type: 3, //ApplicationCommandTypes.USER
 };
-
 
 exports.conf = {
   permLevel: "User",
