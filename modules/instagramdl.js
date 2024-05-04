b73a7f16fdb69853a1c0712116fb304f44336450
arlandrian <alo_istanbul@hotmail.com>
2024-05-05 00:27:49 +0200

    videodl - fixed insta regex again

--- modules/instagramdl.js
+++ modules/instagramdl.js
@@ -3,16 +3,18 @@ const instagramDl = require("@sasmeee/igdl");
 
 // Instagram URL'sini kontrol etmek için regex
 const instagramUrlRegex =
-  /https?:\/\/(?:www.)?instagram.com\/reel\/([^\/?#&]+).*/;
+  /(?:https?:\/\/)?(?:www\.)?instagram\.com\/(p|reel)\/(.+)/;
 
 function isInstagramUrl(url) {
   return instagramUrlRegex.test(url);
 }
 
 async function getVideoStream(url) {
-  const mediaInfo = await instagramDl(url)
-  const resp = await axios.get(mediaInfo[0].download_link, { responseType: "stream" })
-  return resp.data
+  const mediaInfo = await instagramDl(url);
+  const resp = await axios.get(mediaInfo[0].download_link, {
+    responseType: "stream",
+  });
+  return resp.data;
 }
 
 module.exports = { getVideoStream, isInstagramUrl };
